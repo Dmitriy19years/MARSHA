24192304ca8d76ebfb684b341307cfb9b2991502
Dmitriy19years <116866313+Dmitriy19years@users.noreply.github.com>
2022-12-12 08:06:57 +0300

    Add files via upload

---
+++ MARSHA/UserWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+namespace SQLiteApp
+{
+    public partial class UserWindow : Window
+    {
+        public Marsha User { get; private set; }
+        public UserWindow(Marsha user)
+        {
+            InitializeComponent();
+            User = user;
+            DataContext = User;
+        }
+
+        void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
